f21eef09c56d315a1f7d587e3fbafd743ccc2d58
Josh Kirsch <jkirsch@codeaurora.org>
2019-01-17 16:01:08 -0800

    audio-kernel: dsp: Add needed exports
    Add needed exports in q6afe. These are
    needed for sdxprairie drivers where LPASS
    provides the clock.
    
    Change-Id: Icd8256153523c8827ffd65eda1152ef9ad355119
    Signed-off-by: Josh Kirsch <jkirsch@codeaurora.org>

--- dsp/q6afe.c
+++ dsp/q6afe.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -7283,7 +7283,16 @@ int afe_set_lpass_internal_digital_codec_clock(u16 port_id,
 
 	return ret;
 }
-
+/**
+ * afe_enable_lpass_core_shared_clock -
+ *      Configures the core clk on LPASS.
+ *      Need on targets where lpass provides
+ *      clocks
+ * @port_id: afe port id
+ * @enable: enable or disable clk
+ *
+ * Returns success or failure of call.
+ */
 int afe_enable_lpass_core_shared_clock(u16 port_id, u32 enable)
 {
 	struct afe_param_id_lpass_core_shared_clk_cfg clk_cfg;
@@ -7322,7 +7331,16 @@ int afe_enable_lpass_core_shared_clock(u16 port_id, u32 enable)
 	mutex_unlock(&this_afe.afe_cmd_lock);
 	return ret;
 }
+EXPORT_SYMBOL(afe_enable_lpass_core_shared_clock);
 
+/**
+ * q6afe_check_osr_clk_freq -
+ *   Gets supported OSR CLK frequencies
+ *
+ * @freq: frequency to check
+ *
+ * Returns success if freq is supported.
+ */
 int q6afe_check_osr_clk_freq(u32 freq)
 {
 	int ret = 0;
@@ -7347,6 +7365,7 @@ int q6afe_check_osr_clk_freq(u32 freq)
 	}
 	return ret;
 }
+EXPORT_SYMBOL(q6afe_check_osr_clk_freq);
 
 int afe_get_sp_th_vi_ftm_data(struct afe_sp_th_vi_get_param *th_vi)
 {
