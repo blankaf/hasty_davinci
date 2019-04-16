656e1d9b7fdeb03572ef83b0298751dc9acd0919
Derek Chen <chenche@codeaurora.org>
2019-04-15 18:01:45 -0700

    ASoC: msm: Add audio support for sdmshrike on auto platform
    Add audio support for sdmshrike on automotive platform.
    
    Change-Id: I39c70fd7ed66124827f55b7a6e0a834cf735d301
    Signed-off-by: Derek Chen <chenche@codeaurora.org>

--- soc/Android.mk
+++ soc/Android.mk
@@ -12,7 +12,7 @@ AUDIO_SELECT  := CONFIG_SND_SOC_SDM670=m
 endif
 
 ifeq ($(call is-board-platform,msmnile),true)
-ifeq ($(TARGET_PRODUCT), $(filter $(TARGET_PRODUCT), msmnile_au msmnile_gvmq))
+ifeq ($(TARGET_PRODUCT), $(filter $(TARGET_PRODUCT), msmnile_au msmnile_gvmq sdmshrike_au))
 AUDIO_SELECT  := CONFIG_SND_SOC_SA8155=m
 else
 AUDIO_SELECT  := CONFIG_SND_SOC_SM8150=m
@@ -62,7 +62,7 @@ LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 endif
 ###########################################################
-ifneq ($(TARGET_PRODUCT), $(filter $(TARGET_PRODUCT), msmnile_au msmnile_gvmq))
+ifneq ($(TARGET_PRODUCT), $(filter $(TARGET_PRODUCT), msmnile_au msmnile_gvmq sdmshrike_au))
 include $(CLEAR_VARS)
 LOCAL_MODULE              := $(AUDIO_CHIPSET)_pinctrl_wcd.ko
 LOCAL_MODULE_KBUILD_NAME  := pinctrl_wcd_dlkm.ko
