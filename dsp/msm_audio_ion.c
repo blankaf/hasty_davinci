bca685eb8d41c587f9c30d1c298e532139147c94
Xiaojun Sang <xsang@codeaurora.org>
2019-06-27 13:57:57 +0800

    dsp: ion: unmap DMA buffer after kernel ion map failure
    DMA buffer does not unmap after kernel ion map failure. It leads
    to unexpected vaddr and memory leak.
    
    Change-Id: I363dd9178a0bcbca601180bfd0a7bd7ef2430d3e
    Signed-off-by: Xiaojun Sang <xsang@codeaurora.org>

--- dsp/msm_audio_ion.c
+++ dsp/msm_audio_ion.c
@@ -437,6 +437,7 @@ static int msm_audio_ion_map_buf(void *handle, dma_addr_t *paddr,
 	if (rc) {
 		pr_err("%s: ION Get Physical for AUDIO failed, rc = %d\n",
 				__func__, rc);
+		dma_buf_put((struct dma_buf *) handle);
 		goto err;
 	}
 
@@ -444,6 +445,7 @@ static int msm_audio_ion_map_buf(void *handle, dma_addr_t *paddr,
 	if (IS_ERR_OR_NULL(*vaddr)) {
 		pr_err("%s: ION memory mapping for AUDIO failed\n", __func__);
 		rc = -ENOMEM;
+		msm_audio_dma_buf_unmap((struct dma_buf *) handle);
 		goto err;
 	}
 
@@ -514,7 +516,6 @@ int msm_audio_ion_alloc(void **handle, size_t bufsz,
 		if (rc) {
 			pr_err("%s: failed to map ION buf, rc = %d\n", __func__,
 			       rc);
-			dma_buf_put((struct dma_buf*) *handle);
 			goto err;
 		}
 	} else {
@@ -533,7 +534,6 @@ int msm_audio_ion_alloc(void **handle, size_t bufsz,
 
 	memset(*vaddr, 0, bufsz);
 
-	return rc;
 err:
 	return rc;
 }
@@ -778,7 +778,7 @@ int msm_audio_ion_import(void **handle, int fd,
 	rc = msm_audio_ion_map_buf(*handle, paddr, plen, vaddr);
 	if (rc) {
 		pr_err("%s: failed to map ION buf, rc = %d\n", __func__, rc);
-		goto err_ion_flag;
+		goto err;
 	}
 	pr_debug("%s: mapped address = %pK, size=%zd\n", __func__,
 		*vaddr, bufsz);
